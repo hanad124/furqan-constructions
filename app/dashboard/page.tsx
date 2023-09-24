8536d15e1f14fbf5a0f8ffbdcc6d6d7df1ed32c4
hanad124 <hanaddaahir124@gmail.com>
2023-09-25 00:46:38 +0300

    🔥 Implemented advanced summary chart & customers data table in the dashboard page.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -14,6 +14,8 @@ import {
 import Sidebar from "@/components/Sidebar";
 import Navbar from "@/components/Navbar";
 import Widgets from "@/components/Widgets";
+import Charts from "@/components/Charts";
+import DataTable from "@/components/DataTable";
 
 const Dashboard = () => {
   return (
@@ -23,6 +25,8 @@ const Dashboard = () => {
         <div className="p-4 flex-1">
           {/* <Navbar /> */}
           <Widgets />
+          <Charts />
+          <DataTable />
           <div className="p-4 ">
             <div className="grid grid-cols-3 gap-4 mb-4"></div>
             <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
---
+++ components/Charts.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import ReactApexChart from "react-apexcharts";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+import Stack from "@mui/material/Stack";
+import ArrowCircleUpRounded from "@mui/icons-material/ArrowCircleUpRounded";
+
+import { TotalRevenueOptions, TotalRevenueSeries } from "./chart.config";
+
+const ApexChart = () => {
+  return (
+    <div className="mt-5">
+      <Box
+        p={4}
+        flex={1}
+        bgcolor="#fcfcfc"
+        id="chart"
+        display="flex"
+        flexDirection="column"
+        borderRadius="15px"
+      >
+        <Typography fontSize={18} fontWeight={600} color="#11142d">
+          Total Revenue
+        </Typography>
+
+        <Stack my="20px" direction="row" gap={4} flexWrap="wrap">
+          <Typography fontSize={28} fontWeight={700} color="#11142d">
+            $4,805
+          </Typography>
+          <Stack direction="row" alignItems="center" gap={1}>
+            <ArrowCircleUpRounded sx={{ fontSize: 25, color: "#475be8" }} />
+            <Stack>
+              <Typography fontSize={15} color="#475be8">
+                0.8%
+              </Typography>
+              <Typography fontSize={12} color="#808191">
+                Than Last Month
+              </Typography>
+            </Stack>
+          </Stack>
+        </Stack>
+
+        <ReactApexChart
+          series={TotalRevenueSeries}
+          type="bar"
+          height={310}
+          options={TotalRevenueOptions}
+        />
+      </Box>
+    </div>
+  );
+};
+
+export default ApexChart;
