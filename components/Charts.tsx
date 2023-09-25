bae7ebf6c1e89bca14d02c63976c5561ec4b51ab
Hanad Mohamed <hanaddaahir124@gmail.com>
2023-09-25 14:51:43 +0300

    Fix ApexOptions charts bug

--- components/Charts.tsx
+++ components/Charts.tsx
@@ -18,54 +18,55 @@ import { TotalRevenueOptions, TotalRevenueSeries } from "./chart.config";
 
 const ApexChart = () => {
   return (
-    <Box
-      p={4}
-      flex={1}
-      bgcolor="#fcfcfc"
-      id="chart"
-      display="flex"
-      flexDirection="column"
-      borderRadius="15px"
-      className="mt-5 p-5 "
-    >
-      <Typography
-        fontSize={18}
-        fontWeight={600}
-        color="#11142d"
-        className="dark:text-slate-200"
+    <>
+      <Box
+        p={4}
+        flex={1}
+        bgcolor="#fcfcfc"
+        id="chart"
+        display="flex"
+        flexDirection="column"
+        borderRadius="15px"
+        className="mt-5 p-5 "
       >
-        Total Revenue
-      </Typography>
-
-      <Stack my="20px" direction="row" gap={4} flexWrap="wrap">
         <Typography
-          fontSize={28}
-          fontWeight={700}
+          fontSize={18}
+          fontWeight={600}
           color="#11142d"
           className="dark:text-slate-200"
         >
-          $4,805
+          Total Revenue
         </Typography>
-        <Stack direction="row" alignItems="center" gap={1}>
-          <ArrowCircleUpRounded sx={{ fontSize: 25, color: "#475be8" }} />
-          <Stack>
-            <Typography fontSize={15} color="#475be8">
-              0.8%
-            </Typography>
-            <Typography fontSize={12} color="#808191">
-              Than Last Month
-            </Typography>
+
+        <Stack my="20px" direction="row" gap={4} flexWrap="wrap">
+          <Typography
+            fontSize={28}
+            fontWeight={700}
+            color="#11142d"
+            className="dark:text-slate-200"
+          >
+            $4,805
+          </Typography>
+          <Stack direction="row" alignItems="center" gap={1}>
+            <ArrowCircleUpRounded sx={{ fontSize: 25, color: "#475be8" }} />
+            <Stack>
+              <Typography fontSize={15} color="#475be8">
+                0.8%
+              </Typography>
+              <Typography fontSize={12} color="#808191">
+                Than Last Month
+              </Typography>
+            </Stack>
           </Stack>
         </Stack>
-      </Stack>
-
-      {/* <ReactApexChart
+      </Box>
+      <ReactApexChart
         series={TotalRevenueSeries}
         type="bar"
         height={310}
         options={TotalRevenueOptions}
-      /> */}
-    </Box>
+      />
+    </>
   );
 };
 
