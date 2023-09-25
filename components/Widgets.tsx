e768999f6c5b0f01e943c49487dc382100ef1912
Hanad Mohamed <hanaddaahir124@gmail.com>
2023-09-25 14:09:25 +0300

    Fix dashboard widgets bug

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -13,37 +13,37 @@ const Widgets = () => {
   return (
     <div>
       <div className="flex flex-wrap items-center gap-x-2 gap-y-4 w-full">
-        <Card className="flex-1 w-full md:min-w-[12rem] ">
+        <div className="flex-1 w-full md:min-w-[12rem] ">
           <div className="flex items-center justify-between">
-            <CardHeader>
-              <CardTitle>
+            <div>
+              <div>
                 <p className="text-sm text-slate-500 font-normal">Revenue</p>
-              </CardTitle>
-              <CardDescription>
+              </div>
+              <div>
                 <p className="text-2xl text-slate-700 dark:text-slate-200 font-semibold">
                   $4805
                 </p>
                 <p className="text-green-500 mt-1 font-semibold">+2.5%</p>
-              </CardDescription>
-            </CardHeader>
+              </div>
+            </div>
             <div className="w-12 h-12 bg-green-400/40 rounded-lg mr-6 flex justify-center items-center">
               <BiSolidWallet className="text-green-500 text-2xl" />
             </div>
           </div>
-        </Card>
+        </div>
         <Card className="flex-1 w-full md:min-w-[12rem] ">
           <div className="flex items-center justify-between">
-            <CardHeader>
-              <CardTitle>
+            <div>
+              <div>
                 <p className="text-sm text-slate-500 font-normal">Expense</p>
-              </CardTitle>
-              <CardDescription>
+              </div>
+              <div>
                 <p className="text-2xl text-slate-700 dark:text-slate-200 font-semibold">
                   $4805
                 </p>
                 <p className="text-green-500 mt-1 font-semibold">+2.5%</p>
-              </CardDescription>
-            </CardHeader>
+              </div>
+            </div>
             <div className="w-12 h-12 bg-yellow-400/40 rounded-lg mr-6 flex justify-center items-center">
               <BiSolidWallet className="text-yellow-500 text-2xl" />
             </div>
@@ -51,17 +51,17 @@ const Widgets = () => {
         </Card>
         <Card className="flex-1 w-full md:min-w-[12rem] ">
           <div className="flex items-center justify-between">
-            <CardHeader>
-              <CardTitle>
+            <div>
+              <div>
                 <p className="text-sm text-slate-500 font-normal">Sales</p>
-              </CardTitle>
-              <CardDescription>
+              </div>
+              <div>
                 <p className="text-2xl text-slate-700 dark:text-slate-200 font-semibold">
                   $4805
                 </p>
                 <p className="text-green-500 mt-1 font-semibold">+2.5%</p>
-              </CardDescription>
-            </CardHeader>
+              </div>
+            </div>
             <div className="w-12 h-12 bg-violet-400/40 rounded-lg mr-6 flex justify-center items-center">
               <BiSolidCart className="text-violet-500 text-2xl" />
             </div>
@@ -69,17 +69,17 @@ const Widgets = () => {
         </Card>
         <Card className="flex-1 w-full md:min-w-[12rem] ">
           <div className="flex items-center justify-between">
-            <CardHeader>
-              <CardTitle>
+            <div>
+              <div>
                 <p className="text-sm text-slate-500 font-normal">Purchase</p>
-              </CardTitle>
-              <CardDescription>
+              </div>
+              <div>
                 <p className="text-2xl text-slate-700 dark:text-slate-200 font-semibold">
                   $4805
                 </p>
                 <p className="text-red-500 mt-1 font-semibold">+2.5%</p>
-              </CardDescription>
-            </CardHeader>
+              </div>
+            </div>
             <div className="w-12 h-12 bg-red-400/40 rounded-lg mr-6 flex justify-center items-center">
               <BiSolidCart className="text-red-500 text-2xl" />
             </div>
